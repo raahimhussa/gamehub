29305924983c5eef3dc679a301296cedf9f6430f
raahimhussa <raahimhussain1420@gmail.com>
2025-07-21 22:59:41 +0500

    fixed changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Game App',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'Game.dev',
 }
 
 export default function RootLayout({
